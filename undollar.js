9615c1abf1008aec4a9f015e917e3896815faba4
Max Tyrrell <maxtyrrell0@gmail.com>
2017-12-25 20:19:08 +0200

    Write undollar in JavaScript

---
+++ undollar.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process')
+
+// Run a command with its arguments by spawning a process and
+// transparently routing stdio and signals through this parent process
+const spawnedProcess = spawn(
+    process.argv[2],
+    process.argv.slice(3),
+    {stdio: 'inherit'}
+);
+
+// Route signals through so they still reach the spawned process
+process.on('SIGTERM', () => spawnedProcess.kill('SIGTERM'))
+process.on('SIGINT', () => spawnedProcess.kill('SIGINT'))
+process.on('SIGBREAK', () => spawnedProcess.kill('SIGBREAK'))
+process.on('SIGHUP', () => spawnedProcess.kill('SIGHUP'))
+
+// Exit this process when the spawned process exits
+spawnedProcess.on('exit', process.exit)
